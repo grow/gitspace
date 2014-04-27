bd70f1df9b749a8778a9e0b04d9994a6a07dd004
jeremy <jeremy@leftfieldlabs.com>
2014-04-26 20:30:18 -0700

    Update.

--- gitspace/main.py
+++ gitspace/main.py
@@ -4,18 +4,21 @@ from dulwich import repo
 from dulwich import server
 
 
+os.environ['GITSPACE_ROOT'] = 'gitroot'
+
+
 class Backend(server.Backend):
 
   @classmethod
   def open_repository(cls, path):
+    assert '..' not in path
     path = path.lstrip('/')
+    path = path.rstrip('.git')
+    path = os.path.join(os.getenv('GITSPACE_ROOT', path))
     return repo.Repo(path)
 
 
 def get_git_app(environ):
-  if environ['REQUEST_METHOD'] == 'GET':
-    print environ['PATH_INFO']
-  repo.MemoryRepo.init_bare([], {})
   return web.HTTPGitApplication(Backend())
 
 
